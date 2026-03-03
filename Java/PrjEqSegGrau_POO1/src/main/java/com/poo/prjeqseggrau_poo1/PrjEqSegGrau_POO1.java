a6704d14b1b887f29d1b4afe7e548506493e2977
Gustavo Anthony <gustavo.anthony.dos.santos@gmail.com>
2026-03-02 21:34:12 -0300

    Add files via upload

---
+++ Java/PrjEqSegGrau_POO1/src/main/java/com/poo/prjeqseggrau_poo1/PrjEqSegGrau_POO1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.poo.prjeqseggrau_poo1;
+import java.util.Scanner;
+public class PrjEqSegGrau_POO1 {
+
+    public static void main(String[] args) {
+        DadosEntrada de = new DadosEntrada();
+        de.setA(1);
+        de.setB(5);
+        de.setC(6);
+   
+        System.out.println("Dados de Entrada");
+        System.out.println("A: "+de.getA());
+        System.out.println("B: "+de.getB());
+        System.out.println("C: "+de.getC());
+        CalcularEqSegGrau esg = new CalcularEqSegGrau();
+        DadosSaida ds = esg.delta(de);
+        esg.x1l(de);
+        esg.x12(de);
+        esg.xV(de);
+        esg.yV(de);
+        System.out.println("Delta: "+ds.getDelta());
+        System.out.println("X¹: "+ds.getX1l());
+        System.out.println("X²: "+ds.getX2l());
+        System.out.println("xV: "+ds.getxV());
+        System.out.println("yV: "+ds.getyV());     
+    }
+}
+
